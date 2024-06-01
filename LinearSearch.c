713ed6dc5b35c3c5431c33fa048c95a016193e47
Tasnia <164462424+Tasnia22@users.noreply.github.com>
2024-06-02 00:14:00 +0600

    LinearSearch

--- LinearSearch.c
+++ LinearSearch.c
@@ -1,28 +1,21 @@
 #include<stdio.h>
-#define SIZE 5
-
-int LS(int array[],int Size,int key)
+int search(int arr[],int n,int x)
 {
 
-    int i;
-
-    for(i=0;i<SIZE; i++)
-        if(array[i] == key)
-         return 1;
-
-    return 0;
+    for(int i= 0;i<n;i++)
+        if(arr[i]== x)
+        return i;
+    return -1;
 }
-
 int main()
 {
-    int elements[SIZE]={23,7,80,92,0};
-     int Key = 9;
 
-     if(LS(elements,SIZE,Key)== 1)
-        printf("Search found");
-     else
-        printf("Search not found");
-
-     return 0;
+    int arr[]={200,-1,0,36,33};
+    int x= 99;
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int result = search(arr,n,x);
+    (result==-1)
+    ? printf("Element is not found\n")
+    : printf("Element is present at index :%d\n",result);
+    return 0;
 }
-
